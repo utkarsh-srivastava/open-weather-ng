7b63b1011ff881cb15a33d887b3f4b072901afac
utkarsh-srivastava <srivastava.utk@gmail.com>
2021-05-17 00:26:23 +0530

    added route exception handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { ListingsComponent } from './listings/listings.component';
 import { ListingComponent } from './listing/listing.component'
 
 const routes: Routes = [
+  { path: '', redirectTo:'listings', pathMatch:'full'},
   { path: 'listings', component: ListingsComponent},
   { path: 'details/:name', component: ListingComponent},
+  { path: '**', redirectTo:'listings'},
 ];
 
 @NgModule({
