55d068752723cdf59c290089c7984ba0cc98a46d
utkarsh-srivastava <srivastava.utk@gmail.com>
2021-05-16 16:01:49 +0530

    added header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListingsComponent } from './listings/listings.component';
 import { ListingComponent } from './listing/listing.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListingsComponent,
-    ListingComponent
+    ListingComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
