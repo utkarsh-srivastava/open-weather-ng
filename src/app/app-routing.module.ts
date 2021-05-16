50a757d3aa365457b594718a5fb43d6f3b8f8f6e
utkarsh-srivastava <srivastava.utk@gmail.com>
2021-05-16 15:43:57 +0530

    added routes for listings and list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingsComponent } from './listings/listings.component';
+import { ListingComponent } from './listing/listing.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listings', component: ListingsComponent},
+  { path: 'listing', component: ListingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
